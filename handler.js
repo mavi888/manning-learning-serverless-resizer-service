59fcb7d3cb5825790d3c2fbfb0bad1d178dd86e6
marcia.villalba <marcia.villalba@gmail.com>
2017-11-02 17:49:51 +0200

    3.4 - Do it yourself exercise

--- handler.js
+++ handler.js
@@ -20,3 +20,14 @@ module.exports.resizer = (event, context, callback) => {
       callback(error);
     });
 };
+
+module.exports.thumbnails = (event, context, callback) => {
+  const bucket = event.Records[0].s3.bucket.name;
+  const key = event.Records[0].s3.object.key;
+
+  console.log(bucket);
+  console.log(key);
+
+  console.log(`A new file ${key} was created in the bucket ${bucket}`);
+  callback(null, { message: `A new file ${key} was created in the bucket ${bucket}` });
+};
