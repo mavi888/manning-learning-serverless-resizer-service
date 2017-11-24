e9f6df9e66767ec27e27b1c8e21ca1216126c06a
marcia.villalba <marcia.villalba@gmail.com>
2017-11-24 14:39:39 +0200

    4.4 -  AWS Lambda and Step Functions

--- handler.js
+++ handler.js
@@ -1,13 +1,15 @@
 'use strict';
 
+const AWS = require('aws-sdk');
+const stepfunctions = new AWS.StepFunctions();
+
 const resizer = require('./resizer');
 
 module.exports.resizer = (event, context, callback) => {
-  console.log(event.Records[0].s3);
+  console.log(event);
 
-  const bucket = event.Records[0].s3.bucket.name;
-  const key = event.Records[0].s3.object.key;
-
+  const bucket = event.bucketName;
+  const key = event.objectKey;
   console.log(`A file named ${key} was put in a bucket ${bucket}`);
 
   resizer(bucket, key)
@@ -21,6 +23,16 @@ module.exports.resizer = (event, context, callback) => {
     });
 };
 
+module.exports.saveImageMetadata = (event, context, callback) => {
+  console.log(event);
+
+  const bucket = event.bucketName;
+  const key = event.objectKey;
+
+  console.log('saveImageMetadata was called');
+  // TODO
+};
+
 module.exports.thumbnails = (event, context, callback) => {
   const bucket = event.Records[0].s3.bucket.name;
   const key = event.Records[0].s3.object.key;
@@ -30,4 +42,41 @@ module.exports.thumbnails = (event, context, callback) => {
 
   console.log(`A new file ${key} was created in the bucket ${bucket}`);
   callback(null, { message: `A new file ${key} was created in the bucket ${bucket}` });
+};
+
+module.exports.executeStepFunction = (event, context, callback) => {
+  const stateMachineName = 'ImageProcessingMachine'; // The name of the step function we defined in the serverless.yml
+
+  console.log('Fetching the list of available workflows');
+
+  stepfunctions
+    .listStateMachines({})
+    .promise()
+    .then(listStateMachines => {
+      console.log('Searching for the step function');
+
+      for (var i = 0; i < listStateMachines.stateMachines.length; i++) {
+        const item = listStateMachines.stateMachines[i];
+
+        if (item.name.indexOf(stateMachineName) >= 0) {
+          console.log('Found the step function');
+
+          // The event data contains the information of the s3 bucket and the key of the object
+          const eventData = event.Records[0];
+
+          var params = {
+            stateMachineArn: item.stateMachineArn,
+            input: JSON.stringify({ objectKey: eventData.s3.object.key, bucketName: eventData.s3.bucket.name })
+          };
+
+          console.log('Start execution');
+          stepfunctions.startExecution(params).promise().then(() => {
+            return context.succeed('OK');
+          });
+        }
+      }
+    })
+    .catch(error => {
+      return context.fail(error);
+    });
 };
